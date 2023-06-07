77234b3305280e59a809b0b03da9c42aee3905d8
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-07 11:09:47 +0530

    outputs

---
+++ 02-outputs/output.tf
@@ -0,0 +1,11 @@
+# Any string in terraform need to be double quoted
+#single quotes are not supported 
+
+output "sample" {
+    value = "hellow world"
+}
+
+# numbers and booleans{true, false}
+output "sample1" {
+    value = 1000
+}
