12af9de0bf92f328eab08477d431ee126901d1bb
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-08 11:22:25 +0530

    fr

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -7,3 +7,30 @@ data "aws_ec2_spot_price" "example" {
     values = ["Linux/UNIX"]
   }
 }
+
+output "price" {
+    value = data.aws_ec2_spot_price.example.spot_price
+}
+
+
+data "aws_security_group" "selected" {
+    name = "allow-all"
+}
+
+output "sgi" {
+    value = data.aws_security_group.selected.id
+}
+
+data "aws_ami" "example" {
+    owners = ["amazon"]
+    most_recent = true
+    name_regex = "Centos-8-Devops-Practice"
+}
+
+output "ami" {
+  value = data.aws_ami.example.id
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
