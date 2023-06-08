84680bbda237f4711ad1a3fd99b224611e8e0f8b
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-08 18:48:58 +0530

    dd

---
+++ provisionar/main.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+provisioner "remote-exec" {
+   connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+    inline = [
+      "sudo lab auto ansibe",
+      "ansible-pull -i localhost, -U https://github.com/Rajkumar221/roboshop-ansible.git -e main.yml -e roll_name=frontend ",
+    ]
+  }
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
