6c62ff6149df0f58c97e30c1eaeb89f5e5476171
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-08 18:12:14 +0530

    s

--- module1/main.tf
+++ module1/main.tf
@@ -7,3 +7,11 @@ module "sample2" {
   source = "./ec2"
   name = "sample2"
 }
+
+output "public_ip_sample1" {
+  value = module.sampl1.public_ip
+}
+
+output "public_ip_sample2" {
+  value = module.sampl1.public_ip
+}
